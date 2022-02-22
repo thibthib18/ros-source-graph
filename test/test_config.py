adfd299b998a2728d1391c11b223183eebd61d24
thib <thibault@seervision.com>
2022-02-22 20:49:14 +0100

    update test config for log_parser

--- test/test_config.py
+++ test/test_config.py
@@ -4,3 +4,5 @@ from src.config import config
 
 test_config = config
 test_config['target_dir'] = './test/test_directory'
+test_config['log_dir'] = './test/logs/latest'
+test_config['source_graph_location'] = './test/ros_source_graph.json'
